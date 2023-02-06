e4c2dfa7de63b8cb67047bc43ac9983ffab2b5ab
hitch909 <evgenij909@yandex.ru>
2023-02-06 20:08:28 +0500

    47 задача готова

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Задача 47.Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+Console.Clear();
+double[,] array = new double[5, 10];
+void FillArray(double[,] args)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+            Console.Write("{0,6:F2}\t", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
